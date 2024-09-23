a470d667cdbe6b1150019c1407e8d06b16f0e3bf
manu <manuel.giambi@01s.it>
2024-09-23 20:19:20 +0800

    code improvements

--- src/language/zh/index.js
+++ src/language/zh/index.js
@@ -2,55 +2,46 @@ let itemloc = new Array(10);
 let itemcount = new Array(10);
 let itemlocbig = new Array(10);
 let memorycount = new Array(10);
-let differentAnswer = new Array(7);
-let f = new Array(9);
+let f = Array(9).fill(0);
 let question = 1;
 let answer = 2;
 let ques = 1;
 let answ = 2;
 
-let left;
+let remainingItems;
 let score = 0;
 let level;
-let itemnumber;
+let totalItems;
 let notyet = true;
 let donothing = 0;
 
 let overscore = 0;
 let badlevel = 1;
-let itemlocstated;
-let kanawereusing;
-
-let again;
-let x;
-let o;
-let c;
-let h;
 
 function batsu() {
     console.log('batsu');
-    document.getElementById("unknown").innerHTML = "<img id='image' src='../../images/batsu.GIF' name='batsu'>";
+    document.getElementById("hint").innerHTML = "<img id='image' src='../../images/batsu.GIF' alt='batsu'>";
 }
 
 function maru() {
     console.log('maru');
-    document.getElementById("unknown").innerHTML = "<img id='image' src='../../images/maru.GIF' name='maru'>";
+    document.getElementById("hint").innerHTML = "<img id='image' src='../../images/maru.GIF' alt='maru'>";
 }
 
 function cleare() {
     console.log('cleare');
-    document.getElementById("unknown").innerHTML = "";
+    document.getElementById("hint").innerHTML = "";
 }
 
 function cleark() {
     console.log('cleark');
-    document.getElementById("unknown").innerHTML = " ";
+    document.getElementById("hint").innerHTML = " ";
 }
 
 function checkanswer(e) {
     console.log('checkanswer');
-    if (notyet == false) {
-        if (e == x) yes();
+    if (!notyet) {
+        if (e === x) yes();
         else no();
     }
 }
@@ -64,9 +55,9 @@ function yes() {
     maru();
     setTimeout(quizproceed, 200);
 
-    left = left - 1;
-    itemcount[h] = itemcount[h] - 1;
-    overscore = overscore + 1;
+    remainingItems--;
+    itemcount[h]--;
+    overscore++;
 }
 
 function no() {
@@ -78,19 +69,15 @@ function no() {
     batsu();
     setTimeout(quizproceed, 200);
 
-    left = left + 1;
-    itemcount[h] = itemcount[h] + 1;
+    remainingItems++;
+    itemcount[h]++;
 }
 
 function choice() {
     console.log('choice');
-    var g = 0;
-    while (g == 0) {
-        h = Math.random();
-        h = h * itemnumber;
-        h = Math.ceil(h);
-        g = itemcount[h];
-    }
+    do {
+        h = Math.ceil(Math.random() * totalItems);
+    } while (itemcount[h] === 0);
     x = itemloc[h];
 }
 
@@ -99,18 +86,16 @@ function otherchoice() {
     let i = 0;
     let done = 0;
 
-    while (done != 1) {
+    while (done !== 1) {
         done = 1;
-        i = Math.random();
-        i = i * itemnumber;
-        i = Math.ceil(i);
+        i = Math.ceil(Math.random() * totalItems);
         o = itemloc[i];
-        for (i = 1; i <= 8; i++) {
-            if (o == f[i]) {
+        for (let j = 1; j <= 8; j++) {
+            if (o === f[j]) {
                 done = 0;
             }
-            if (f[i] > 0) {
-                if (hz[o][1][0] == hz[f[i]][1][0]) {
+            if (f[j] > 0) {
+                if (hz[o][1][0] === hz[f[j]][1][0]) {
                     done = 0;
                 }
             }
@@ -120,12 +105,9 @@ function otherchoice() {
 
 function choosetranslation(file) {
     console.log('choosetranslation');
-    let trans = Math.random();
+    let trans = Math.floor(Math.random() * hz[file][1].length);
 
-    trans = trans * (hz[file][1].length);
-    trans = Math.floor(trans);
-
-    if (file == x) {
+    if (file === x) {
         trans = itemlocbig[h];
     }
     return trans;
@@ -133,71 +115,67 @@ function choosetranslation(file) {
 
 function answerSlot(button) {
     console.log('answerSlot');
-    if (c == button)
-        return 1;
-    return 0;
+    return c === button ? 1 : 0;
 }
 
 function crosshash(ovalue, xvalue) {
     console.log('crosshash');
-    for (let countx = 0; countx <= 4; countx = countx + 1) {
-        for (let counto = 0; counto <= 4; counto = counto + 1) {
-            if ((hz[ovalue][counto]) == (hz[xvalue][countx])) {
+    for (let countx = 0; countx <= 4; countx++) {
+        for (let counto = 0; counto <= 4; counto++) {
+            if ((hz[ovalue][counto]) === (hz[xvalue][countx])) {
                 again = 1;
             }
         }
     }
-    if (again == 1) {
+    if (again === 1) {
         again = 0;
         otherchoice();
-        crosshash(o, x);
+        if (o !== x) {
+            crosshash(o, x);
+        }
     }
 }
 
 function choices(x) {
     console.log('choices');
-    for (let i = 1; i <= 8; i++) {
-        f[i] = 0
-    }
+    f.fill(0);
     again = 0;
-    c = Math.random();
-    c *= 8;
-    c = Math.ceil(c);
+    c = Math.ceil(Math.random() * 8);
 
     for (let j = 1; j < 9; j++) {
-        if (answerSlot(j) != 0) {
-            if (answer == 3) {
+        if (answerSlot(j) !== 0) {
+            if (answer === 3) {
                 document.getElementById(j).innerHTML = hz[x][2];
             }
-            if (answer == 2) {
+            if (answer === 2) {
                 let trans = choosetranslation(x);
                 document.getElementById(j).innerHTML = hz[x][1][trans];
             }
-            if (answer == 1) {
+            if (answer === 1) {
                 document.getElementById(j).innerHTML = hz[x][0];
             }
             f[j] = x;
-        }
-        else {
+        } else {
             let chooseAgain = 1;
-            while (chooseAgain == 1) {
+            while (chooseAgain === 1) {
                 chooseAgain = 0;
                 otherchoice();
                 crosshash(o, x);
-                if (answer == 3) {
+                if (answer === 3) {
                     document.getElementById(j).innerHTML = hz[o][2];
                 }
-                if (answer == 2) {
+                if (answer === 2) {
                     let trans = choosetranslation(o);
                     document.getElementById(j).innerHTML = hz[o][1][trans];
                 }
-                if (answer == 1)
+                if (answer === 1) {
                     document.getElementById(j).innerHTML = hz[o][0];
+                }
             }
             f[j] = o;
         }
     }
-    if (again == 1) {
+    if (again === 1) {
         choices(x);
     }
 }
@@ -210,18 +188,20 @@ function setformat(que, ans) {
 
 function sameway() {
     console.log('sameway');
-    left = itemnumber;
-    document.getElementById("leftfield").innerHTML = left;
+    remainingItems = totalItems;
+    document.getElementById("leftfield").innerHTML = remainingItems;
 
     overscore = 0;
-    for (let i = itemnumber; i > 0; i--)
-        itemcount[i] = 1
+    for (let i = totalItems; i > 0; i--) {
+        itemcount[i] = 1;
+    }
     score = 1;
-    if (donothing != 1)
-        newkanji();
+    if (donothing !== 1) {
+        newhanzi();
+    }
 }
 
-function newkanji() {
+function newhanzi() {
     console.log('newkanji');
     cleark();
     cleare();
@@ -230,15 +210,15 @@ function newkanji() {
 
     choice();
 
-    if (question == 3) {
-        document.getElementById("unknown").innerHTML = hz[x][2];
+    if (question === 3) {
+        document.getElementById("hint").innerHTML = hz[x][2];
     }
-    if (question == 2) {
+    if (question === 2) {
         let trans = choosetranslation(x);
-        document.getElementById("unknown").innerHTML = hz[x][1][trans];
+        document.getElementById("hint").innerHTML = hz[x][1][trans];
     }
-    if (question == 1) {
-        document.getElementById("unknown").innerHTML = hz[x][0];
+    if (question === 1) {
+        document.getElementById("hint").innerHTML = hz[x][0];
     }
 
     document.getElementById("debugfield").innerHTML = h;
@@ -251,179 +231,145 @@ function startquiz() {
     answer = answ;
     badlevel = 1;
 
-    while (badlevel == 1) {
+    while (badlevel === 1) {
         badlevel = 1;
 
         level = prompt('Change level: 1-166, or 1st-4th, or all');
 
-        if (parseInt(level) == level && parseInt(level) < 165) {
+        if (parseInt(level) === level && parseInt(level) < 165) {
             badlevel = 0;
-            itemnumber = Math.min(15);
-            left = itemnumber;
+            totalItems = Math.min(15);
+            remainingItems = totalItems;
 
-            for (let levelplacecount = 1; levelplacecount < left + 1; levelplacecount = levelplacecount + 1) {
+            for (let levelplacecount = 1; levelplacecount < remainingItems + 1; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount + level * 15 - 15;
-                itemlocstated = levelplacecount + level * 15 - 15;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
         if (
-            level == '100s' || level == '200s' || level == '300s' ||
-            level == '400s' || level == '500s' || level == '600s' ||
-            level == '700s' || level == '800s' || level == '900s' || level == '1000s'
+            level === '100s' || level === '200s' || level === '300s' ||
+            level === '400s' || level === '500s' || level === '600s' ||
+            level === '700s' || level === '800s' || level === '900s' || level === '1000s'
         ) {
             badlevel = 0;
-            itemnumber = 100;
-            left = itemnumber;
-            for (let levelplacecount = 1; levelplacecount < left + 1; levelplacecount = levelplacecount + 1) {
+            totalItems = 100;
+            remainingItems = totalItems;
+            for (let levelplacecount = 1; levelplacecount < remainingItems + 1; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount + level.replace('s', '') * 1 - 100;
-                itemlocstated = levelplacecount + level.replace('s', '') * 1 - 100;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        if (level == '1st') {
+        if (level === '1st') {
             badlevel = 0;
-            left = 322;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 322;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount;
-                itemlocstated = levelplacecount;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        if (level == '2nd') {
+        if (level === '2nd') {
             badlevel = 0;
-            left = 180;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 180;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount + 322;
-                itemlocstated = levelplacecount + 322;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        if (level == '3rd') {
+        if (level === '3rd') {
             badlevel = 0;
-            left = 497;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 497;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount + 322 + 180;
-                itemlocstated = levelplacecount + 322 + 180;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        if (level == '4th') {
+        if (level === '4th') {
             badlevel = 0;
-            left = 1482;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 1482;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount + 322 + 180 + 497;
-                itemlocstated = levelplacecount + 322 + 180 + 497;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        // if (level == '5th') {
-        //     badlevel = 0;
-        //     left = 185;
-        //     itemnumber = left;
-        //     for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
-        //         itemloc[levelplacecount] = levelplacecount + 639;
-        //         itemlocstated = levelplacecount + 639;
-        //         itemcount[levelplacecount] = 1;
-        //         memorycount[levelplacecount] = 1;
-        //     }
-        // }
-        //
-        // if (level == '6th') {
-        //     badlevel = 0;
-        //     left = 181;
-        //     itemnumber = left;
-        //     for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
-        //         itemloc[levelplacecount] = levelplacecount + 824;
-        //         itemlocstated = levelplacecount + 824;
-        //         itemcount[levelplacecount] = 1;
-        //         memorycount[levelplacecount] = 1;
-        //     }
-        // }
-
-        if (level == '1-2') {
+        if (level === '1-2') {
             badlevel = 0;
-            left = 502;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 502;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount;
-                itemlocstated = levelplacecount;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        if (level == '1-3') {
+        if (level === '1-3') {
             badlevel = 0;
-            left = 999;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 999;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount;
-                itemlocstated = levelplacecount;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-
-        if (level == '1-4' || level == 'all')  {
+        if (level === '1-4' || level === 'all') {
             badlevel = 0;
-            left = 2481;
-            itemnumber = left;
-            for (let levelplacecount = 1; levelplacecount <= left; levelplacecount = levelplacecount + 1) {
+            remainingItems = 2481;
+            totalItems = remainingItems;
+            for (let levelplacecount = 1; levelplacecount <= remainingItems; levelplacecount++) {
                 itemloc[levelplacecount] = levelplacecount;
-                itemlocstated = levelplacecount;
                 itemcount[levelplacecount] = 1;
                 memorycount[levelplacecount] = 1;
             }
         }
 
-        if (level == "" || level == " " || level == "undefined" || level == null) {
+        if (level === "" || level === " " || level === "undefined" || level === null) {
             badlevel = 0;
-            left = 0;
+            remainingItems = 0;
             donothing = 1;
         }
-        if (badlevel == 1) {
+        if (badlevel === 1) {
             alert('Invalid level. Try again');
         }
     }
     score = 1;
-    if (donothing != 1) {
+    if (donothing !== 1) {
         document.getElementById("levelfield").innerHTML = level;
-        newkanji();
+        newhanzi();
     }
-    document.getElementById("leftfield").innerHTML = left;
+    document.getElementById("leftfield").innerHTML = remainingItems;
     notyet = false;
 
-    if (ques == 1) {
-        document.getElementById("unknown").className = "bigText";
-    } else if (ques == 2) {
-        document.getElementById("unknown").className = "smallText";
-    } else if (ques == 3) {
-        document.getElementById("unknown").className = "mediumText";
+    if (ques === 1) {
+        document.getElementById("hint").className = "bigText";
+    } else if (ques === 2) {
+        document.getElementById("hint").className = "smallText";
+    } else if (ques === 3) {
+        document.getElementById("hint").className = "mediumText";
     }
 
-    if (answ == 1) {
+    if (answ === 1) {
         document.getElementById("choicetable").className = "bigText";
-    } else if (answ == 2) {
+    } else if (answ === 2) {
         document.getElementById("choicetable").className = "smallText";
-    } else if (answ == 3) {
+    } else if (answ === 3) {
         document.getElementById("choicetable").className = "mediumText";
     }
 }
@@ -432,20 +378,17 @@ function quizproceed() {
     console.log('quizproceed');
     let ascore;
 
-    document.getElementById("leftfield").innerHTML = left;
+    document.getElementById("leftfield").innerHTML = remainingItems;
 
-    if (left > 0) {
-        score = score + 1;
-        newkanji();
-    }
-    else {
-        score = overscore / score;
-        score = score * 100;
+    if (remainingItems > 0) {
+        score++;
+        newhanzi();
+    } else {
+        score = (overscore / score) * 100;
 
-        if (score != 100) {
+        if (score !== 100) {
             score = Math.floor(score);
-            score = score - 50;
-            score = score * 2;
+            score = (score - 50) * 2;
 
             if (score <= 50) ascore = "F";
             if (score >= 51) ascore = "D";
@@ -453,9 +396,9 @@ function quizproceed() {
             if (score >= 73) ascore = "B";
             if (score >= 83) ascore = "A";
         }
-        if (score == 100) ascore = "PERFECT";
+        if (score === 100) ascore = "PERFECT";
         cleare();
         cleark();
-        document.getElementById("unknown").innerHTML = ascore;
+        document.getElementById("hint").innerHTML = ascore;
     }
 }
